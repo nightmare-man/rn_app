f6822cdbd813864ab17ebba8abeab1e051755216
siminli <1071100387@qq.com>
2020-03-23 11:38:45 +0800

    use react-navigation

--- App.js
+++ App.js
@@ -1,36 +1,46 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { View, Text, Button} from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-export default class App extends React.Component{
-  constructor(){
-    super();
-  }
-  render(){
+class HomeScreen extends React.Component {
+  render() {
     return (
-      <Text style={{color:'blue',fontSize:60}}>Hello world!</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Home1')}/>
+      </View>
+      
+    );
+  }
+}
+class HomeScreen1 extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen1</Text>
+      </View>
+    );
+  }
+}
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+  Home1: {
+    screen: HomeScreen1,
+  },
+},{
+  initialRouteName: 'Home',
+}
+);
+const Container=createAppContainer(AppNavigator);
+export default class App extends React.Component{
+  render(){
+    return(
+      <Container/>
     );
   }
 }
